c6ceb2e18e295099e17b8e2d6f3e9e9af875070f
MrTobyWan <tobiasbrock@gmail.com>
2022-10-20 21:22:41 +0200

    Local storage is working now. :)

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,9 @@
 import { writable } from 'svelte/store';
 
-export const blue = writable<boolean>(false);
-export const red = writable<boolean>(false);
-export const green = writable<boolean>(false);
+export const blue = writable<boolean>(localStorage.blue === 'true');
+export const red = writable<boolean>(localStorage.red === 'true');
+export const green = writable<boolean>(localStorage.green === 'true');
+
+blue.subscribe((value) => localStorage.blue = String(value));
+red.subscribe((value) => localStorage.red = String(value));
+green.subscribe((value) => localStorage.green = String(value));
