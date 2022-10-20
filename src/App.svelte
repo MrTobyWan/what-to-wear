9dee5d441a8a12ca4f4e03b40075a63e157978f5
MrTobyWan <tobiasbrock@gmail.com>
2022-10-20 20:02:27 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ClothesPicker from './lib/ClothesPicker.svelte';
+	// import { timeLeft } from './Counter.svelte';
+
+</script>
+
+<div>
+	<ClothesPicker/>
+</div>
---
+++ src/lib/ClothesPicker.svelte
@@ -0,0 +1,19 @@
+<div class="clothesSelection">
+	<button id="blue"></button>
+	<button id="red"></button>
+	<button id="green"></button>
+</div>
+
+<style>
+	#blue {
+		background-color: blue;
+	}
+
+	#red {
+		background-color: red;
+	}
+
+	#green {
+		background-color: green;
+	}
+</style>
