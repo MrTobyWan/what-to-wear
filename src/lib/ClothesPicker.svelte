4874b066a61ce1652347b8d7af2c74a46870f75c
MrTobyWan <tobiasbrock@gmail.com>
2022-10-25 13:02:42 +0200

    Getting closer to a working save button that saves the store

--- src/lib/ClothesPicker.svelte
+++ src/lib/ClothesPicker.svelte
@@ -1,11 +1,42 @@
 <script lang='ts'>
   import { blue, red, green } from './stores';
+
+	// let clothesSelected: string[] = [];
+
+	// let clothesPool: string[] = [
+	// 	'Red',
+	// 	'Green',
+	// 	'Blue'
+	// ];
+
+	let clothesSelected: {name: string, store: any}[] = [];
+
+	let clothesPool: {name: string, store: any}[] = [
+		{'name': 'Red', 'store': $red},
+		{'name': 'Green', 'store': $green},
+		{'name': 'Blue', 'store': $blue},
+	];
+
+	function saveSelection() {
+		alert('selected clothes: ' + clothesSelected);
+		// for (let i = 0; i < clothesSelected.length; i++) {
+		// 	clothesSelected[i].store = !clothesSelected[i].store;
+		// }
+	}
+
 </script>
 
 <div class='clothesSelection'>
-	<button id='blue' on:click={() => $blue = !$blue}></button>
-	<button id='red' on:click={() => $red = !$red}></button>
-	<button id='green' on:click={() => $green = !$green}></button>
+	{#each clothesPool as clothing}
+		<label>
+			<input type=checkbox bind:group={clothesSelected} value={clothing.name}>
+			{clothing.name}
+		</label>
+	{/each}
+</div>
+
+<div>
+	<button on:click={() => saveSelection()}>Gem</button>
 </div>
 
 <div>
@@ -23,7 +54,7 @@
 </div>
 
 <style>
-	#blue {
+	/* #blue {
 		background-color: blue;
 	}
 
@@ -33,5 +64,5 @@
 
 	#green {
 		background-color: green;
-	}
+	} */
 </style>
