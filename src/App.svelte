af430d3bae7dc3648d3d946fc723ebde7b95d9d0
MrTobyWan <tobiasbrock@gmail.com>
2023-03-06 15:01:57 +0100

    Needed to start over with Sveltekit instead of basic Svelte

--- src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import ClothesPicker from './lib/ClothesPicker.svelte';
-
-</script>
-
-<div>
-	<ClothesPicker/>
-</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	// const getColors = async () => {
+	// 	const res = await fetch('/+page.server.ts');
+	// 	const data = await res.json();
+	// 	console.log(data);
+	// };
+
+	// getColors();
+</script>
+
+<h1>Welcome!</h1>
+
+<ul>
+	<li>Red</li>
+	<li>Green</li>
+	<li>Blue</li>
+</ul>
