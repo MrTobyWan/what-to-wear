e9762b1d26e5dd0a4976fd2bf6d78d3490374c47
MrTobyWan <tobiasbrock@gmail.com>
2023-06-30 14:13:32 +0200

    Back where we started!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
-<script>
+<script lang="ts">
+	let savedColors: string = 'Nothing yet';
 </script>
 
-<h1>Welcome!</h1>
+<h1>Hit "Save" to update the stored colors</h1>
 
-<ul>
-	<li>Red</li>
-	<li>Green</li>
-	<li>Blue</li>
-</ul>
+<div>
+	<label>
+		<input type="checkbox" />
+		Red
+	</label>
+	<label>
+		<input type="checkbox" />
+		Green
+	</label>
+	<label>
+		<input type="checkbox" />
+		Blue
+	</label>
+</div>
+
+<button>Save</button>
+
+<p>Here's what has been saved:</p>
+
+<p style="font-weight: bold;">{savedColors}</p>
