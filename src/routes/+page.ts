e22dbcddb92ae66455a4b6d833e1d83bf28b972e
MrTobyWan <tobiasbrock@gmail.com>
2023-07-05 22:29:35 +0200

    This file will make sure to handle initial load of the app

---
+++ src/routes/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+
+// export const load: PageLoad = async ({ params, fetch }) => {
+// 	const fetchColors = async () => {
+// 		return await collection.findOne({});
+// 	};
+
+// 	return {
+// 		colors: fetchColors();
+// 	};
+// };
