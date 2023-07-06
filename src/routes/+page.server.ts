0ff2931faebc97143b1d3bfbb074d7853c10a407
MrTobyWan <tobiasbrock@gmail.com>
2023-07-06 14:21:14 +0200

    Created the mongoDB and got the object added to the server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,25 @@
 import type { Actions } from './$types';
 import type { PageServerLoad } from './$types';
-import { collection } from '$lib/server/colors';
 
-export const load = (async ({ params }) => {
+import { collection } from '$lib/server/colors';
+import { ObjectId } from 'mongodb';
+import { error } from '@sveltejs/kit';
+
+export const load = (async () => {
 	const fetchColors = async () => {
-		return collection.findOne({});
+		return collection.findOne({ _id: new ObjectId('64a679e69d75fa88bc668d7d') });
 	};
 
+	const colors = await fetchColors();
+
+	if (!colors) {
+		throw error(404, 'not found');
+	}
+
 	return {
-		colors: fetchColors(),
+		red: colors.red,
+		green: colors.green,
+		blue: colors.blue,
 	};
 }) satisfies PageServerLoad;
 
@@ -16,6 +27,8 @@ export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
 
-		console.log(data.get('green'));
+		await collection.updateOne({ _id: new ObjectId('64a679e69d75fa88bc668d7d') }, { $set: { red: data.has('red'), green: data.has('green'), blue: data.has('blue') } });
+
+		return { success: true };
 	},
 } satisfies Actions;
