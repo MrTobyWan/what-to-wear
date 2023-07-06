b68f525134c9d80c3b5dc6e578988d2c1b95d1f3
MrTobyWan <tobiasbrock@gmail.com>
2023-07-06 14:21:37 +0200

    Now with a working database store!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,41 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
 
 	export let data: PageData;
-	let savedColors: string = 'Nothing yet';
 </script>
 
 <h1>Hit "Save" to update the stored colors</h1>
 
-<form method="POST">
+<form method="POST" use:enhance>
 	<label>
-		<input bind:checked={data.red} name="red" type="checkbox" />
+		<input name="red" type="checkbox" />
 		Red
 	</label>
 	<label>
-		<input bind:checked={data.green} name="green" type="checkbox" />
+		<input name="green" type="checkbox" />
 		Green
 	</label>
 	<label>
-		<input bind:checked={data.blue} name="blue" type="checkbox" />
+		<input name="blue" type="checkbox" />
 		Blue
 	</label>
 
-	<button>Save</button>
+	<button>Save to db</button>
 </form>
 
 <div />
 
 <p>Here's what has been saved:</p>
 
-<p style="font-weight: bold;">{savedColors}</p>
+{#if data.red}
+	<p style="font-weight: bold;">Red</p>
+{/if}
+
+{#if data.green}
+	<p style="font-weight: bold;">Green</p>
+{/if}
+
+{#if data.blue}
+	<p style="font-weight: bold;">Blue</p>
+{/if}
